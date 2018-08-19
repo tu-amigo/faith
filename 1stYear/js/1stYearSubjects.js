8f0b7070c887183fa205b1a3be6a57a91834d7e4
Krupesh Anadkat <kmanadkat@gmail.com>
2018-08-19 11:31:56 +0530

    All 3 years Subject Folder Uploaded & Linked

---
+++ 1stYear/js/1stYearSubjects.js
@@ -0,0 +1,34 @@
+//////////////////////////////// Subjects Information ////////////////////////////////
+
+// 1st Year
+//Full Subject Name
+var SubjectCode_Small_letters = {
+    name : '',
+    code : '',
+    credit : 0,
+    ltp : 'L- &nbsp;&nbsp;&nbsp; T- &nbsp;&nbsp;&nbsp; P-',
+    handout : 'url',
+    marks : [
+        { num : 1, test: 'MST', mark: 0},
+        { num : 2, test: 'EST', mark: 0},
+        { num : 3, test: 'Lab/Quiz/Project', mark: 0}
+    ],
+    sheet : [
+        { title : 'Title', link : 'url'}
+    ],
+    slide : [
+        { title : 'Title', link : 'url'}
+    ],
+    lab : [
+        { title : 'Title', link : 'url'}
+    ],
+    paper : [
+        { title : 'Title', link : 'url'}
+    ],
+    book : [
+        { title : 'Title', link : 'url'}
+    ],
+    note : [
+        { title : 'Title', link : 'url'}
+    ]
+};
---
+++ 3rdYear/js/3rdYearSubjects.js
@@ -0,0 +1,186 @@
+//////////////////////////////// Subjects Information ////////////////////////////////
+
+// 3rd Year
+//UCS303 Operating Systems
+var ucs303 = {
+    name : 'UCS303 Operating Systems',
+    code : 'UCS303',
+    credit : 4,
+    ltp : 'L-3 &nbsp;&nbsp;&nbsp; T-0 &nbsp;&nbsp;&nbsp; P-2',
+    handout : 'https://drive.google.com/open?id=1OXjZC_T7Mu7cteG49bNVsfPJXtGbHPuD',
+    marks : [
+        { num : 1, test: 'MST', mark: 25},
+        { num : 2, test: 'EST', mark: 45},
+        { num : 3, test: 'Lab/Quiz', mark: 30}
+    ],
+    slide : [
+        { title : 'Slides By Galvin - 1', link : 'https://drive.google.com/open?id=1WiCabGCYp6kRdbH7TGXDYSsUH6A15dqT'},
+        { title : 'Slides By Galvin - 2', link : 'https://drive.google.com/open?id=1wxfuEOJhKI_O9ZZs0nOVHpJ_GUWEYV_T'},
+        { title : 'Slides By Galvin - 3', link : 'https://drive.google.com/open?id=1CIEokptsgl8AkHMUqDtIG7JwsHRAvrE7'},
+        { title : 'Slides By Galvin - 4', link : 'https://drive.google.com/open?id=1ll0uTGgnTieFkRwKthd2M6vVAo7Z0m5K'},
+        { title : 'Slides By Galvin - 5', link : 'https://drive.google.com/open?id=1VEFmFtfioPGOENbGH-v4PcGVaiquMt7-'},
+        { title : 'Slides By Galvin - 6', link : 'https://drive.google.com/open?id=1dY0cswL2RfWbDE3V5kh_FlxeJHPdxBom'},
+        { title : 'Slides By Galvin - 7', link : 'https://drive.google.com/open?id=1ea-ncgrnENbBJnpz-MzB18IwUgpPWzKb'},
+        { title : 'Slides By Galvin - 8', link : 'https://drive.google.com/open?id=1ZrgWytmvTmEVoYPSid5E4Vpyw-M8UCZV'},
+        { title : 'Slides By Galvin - 9', link : 'https://drive.google.com/open?id=1j7Z1hBB-UaJcArB-5GgDpRHdZGtmD0qX'},
+        { title : 'Slides By Galvin - 10', link : 'https://drive.google.com/open?id=11HwADxKROcwblJ9KDJFV_y28IfnuYdsK'},
+        { title : 'Slides By Galvin - 11', link : 'https://drive.google.com/open?id=1kl_H5rrmaChuDydT9a3Zw3rQdSNSF68e'},
+        { title : 'Slides By Galvin - 12', link : 'https://drive.google.com/open?id=1RqC99-MmF1ujsYgm-1rgUhtsbhVVO8zo'},
+        { title : 'Slides By Galvin - 13', link : 'https://drive.google.com/open?id=1IlMCW5LZ8DtjO41IccNk3aVBzVcnEp3O'},
+        { title : 'Slides By Galvin - 14', link : 'https://drive.google.com/open?id=1p-d_TAkjswvbdeMXohufQNhdPHz1s0kC'},
+        { title : 'Alternative Slides 1', link : 'https://drive.google.com/open?id=1ZOs-OjiPY1yW3aoFzb8MKA8cPWOzRZbn'},
+        { title : 'Alternative Slides 2', link : 'https://drive.google.com/open?id=1AVs-YK9x7afE1ns1Yhmo4LvSZmuqyFhc'},
+    ],
+    lab : [
+        { title : 'Lab Assignment 1 to 5', link : 'https://drive.google.com/open?id=1RA5mlGHd1aI0x1BUrENX05CKDg2qRWFT'},
+        { title : 'Lab Assignment 6 to 7', link : 'https://drive.google.com/open?id=1Ua0O3kmWo89R42kyNBLJN5y1lbYexU5W'},
+        { title : 'Lab Assignment 8 to 10', link : 'https://drive.google.com/open?id=1c_cNX1hTJRw2JK2HfHIgsHl7IRVJRI4P'},
+        { title : 'Lab Assignment 11 to 15', link : 'https://drive.google.com/open?id=1A1BD44CKsz8FSjAXPmYygNJ7P3zC8gxZ'}
+    ],
+    paper : [
+        { title : '2017 MidSem', link : 'https://drive.google.com/open?id=1Nd10rAxeExViv_18YTJpA-3ADFhUvZ-7'},
+        { title : '2016 MidSem', link : 'https://drive.google.com/open?id=1N1c7yKvPy_g6Ynk7OM7vyUKUf28ilT2M'},
+        { title : '2016 EndSem', link : 'https://drive.google.com/open?id=1XEEW8qClfqG210Hk7Dftv-AY-ODfE_2l'},
+        { title : '2017 EndSem', link : 'https://drive.google.com/open?id=1TKNTSBhqQ3ASx1hBATaT2KIFrbTsE0sJ'},
+        { title : '2018 EndSem Aux', link : 'https://drive.google.com/open?id=1acq74l5qrfIuYnJQLsKVLHbGbYL-B1zC'},
+    ],
+    book : [
+        { title : 'Galvin', link : 'https://drive.google.com/open?id=1yv4bVUb3Gs3C7EIoi_-zbF9l3gm7aiSa'},
+        { title : 'Dhamdere OS', link : 'https://drive.google.com/open?id=1ldrOhOMCzS3_pL9LdtBd3csdCPRtgvzV'},
+        { title : 'Sumitabhas Lab', link : 'https://drive.google.com/open?id=1ts8K906vUYvx80H_phv_O3d8IwaC2hfM'},
+        { title : 'William Stallings', link : 'https://drive.google.com/open?id=1-VtxI7IXRX7kOApzxYW2_OpED71Wq_31'}
+    ],
+};
+
+//UEC401 Analog Communication Systems
+var uec401 = {
+    name : 'UEC401 Analog Communication Systems',
+    code : 'UEC401',
+    credit : 4.5,
+    ltp : 'L-3 &nbsp;&nbsp;&nbsp; T-1 &nbsp;&nbsp;&nbsp; P-2',
+    handout : 'https://drive.google.com/open?id=1xP7VfAE63YGnrXIFm8jPnHIVszh3wbHL',
+    marks : [
+        { num : 1, test: 'MST', mark: 25},
+        { num : 2, test: 'EST', mark: 40},
+        { num : 3, test: 'Lab/Tut/Project', mark: 35}
+    ],
+    sheet : [
+        { title : 'Tutorial Sheet 1', link : 'https://drive.google.com/open?id=1MpVaEWtMlGWrZ7fdgzLFSfhbEzMeQYBU'},
+        { title : 'Tutorial Sheet 2', link : 'https://drive.google.com/open?id=1uI6MpO-thcG8AmnatsHD8r7mvwNmoppy'},
+        { title : 'Tutorial Sheet 3', link : 'https://drive.google.com/open?id=1-pDKJ0Ocs9HhAcWA69uyFCLKw5PtF86x'},
+        { title : 'Tutorial Sheet 4', link : 'https://drive.google.com/open?id=1BeTWDO2989kiRfgN5sJrbL4erybu2oPO'}
+    ],
+    lab : [
+        { title : 'Amplitude Modulation', link : 'https://drive.google.com/open?id=1iYb7RPxEo1Z5TzZ5q-Xm8FxBJLI1jUtn'}
+    ],
+    paper : [
+        { title : '2017 MidSem', link : 'https://drive.google.com/open?id=17f0GI8-wRHXtdI6uTTJy_Am0CtiFU938'},
+        { title : '2016 MidSem', link : 'https://drive.google.com/open?id=1y1vlbOTHvLywXFij8CYdWs-gobKDFdtV'},
+        { title : '2017 EndSem', link : 'https://drive.google.com/open?id=16_FLn9nv3C7j1SjBgctzBZJZga3SaV_I'}
+    ],
+    book : [
+        { title : 'George Kenndy' , link : 'https://drive.google.com/open?id=1yue1cXleOvCtuu5o-AoeYNgyNcvwbdxX'}
+    ],
+    slide :[
+        { title : 'Slide 1', link : 'https://drive.google.com/open?id=1DY7i0eYgrxNdkPNOAUiouljxeAY0ku5R'},
+        { title : 'Slide 2', link : 'https://drive.google.com/open?id=1EriA9M2Ghlx5DUfk62hS87s4k3F-VLie'},
+        { title : 'Slide 3', link : 'https://drive.google.com/open?id=1AB8_6eRMSfzak2WdKpsO1j3i3aUaKqPg'}
+    ],
+    note : [
+        { title : 'Notes 1', link : 'https://drive.google.com/open?id=1gRgxGet1vYWYRzjVEtUyRlH4B0stqt6n'},
+        { title : 'Notes 2', link : 'https://drive.google.com/open?id=1KnKLHSsg5jGxP_dfcmlyzEMcXeqx16Op'},
+        { title : 'Notes 3', link : 'https://drive.google.com/open?id=1tIjN1Cw3cdT5H91SJjFQ_63kt-098FuH'},
+        { title : 'Notes 4', link : 'https://drive.google.com/open?id=1haMZQKHYRQ4lK43mpfc6yjkOhg-Xf_0K'},
+        { title : 'Notes 5', link : 'https://drive.google.com/open?id=1NCiLQ9nVgOzwNZwcZcfylnraJBgfDUik'},
+        { title : 'Notes 6', link : 'https://drive.google.com/open?id=1qCD9wzP_glWRkdOozK9gJpCHUV_JsWOX'},
+        { title : 'Notes 7', link : 'https://drive.google.com/open?id=1FrAz8YTHaZhImGh5cIx5Jqks-Zy-_r9n'},
+        { title : 'Notes 8', link : 'https://drive.google.com/open?id=1HgT-GP68vgSaumbZ_hZrLl-eUOqiG2kE'},
+    ]
+};
+
+//UEC612 Digital System Design
+var uec612 = {
+   name : 'UEC612 Digital System Design',
+   code : 'UEC612',
+   credit : 4.5,
+   ltp : 'L-3 &nbsp;&nbsp;&nbsp; T-1 &nbsp;&nbsp;&nbsp; P-2',
+   handout : 'https://drive.google.com/open?id=17Z7VkqsaLtpCLauAcC_PypzjjDg2whzO',
+   marks : [
+       { num : 1, test: 'MST', mark: 25},
+       { num : 2, test: 'EST', mark: 35},
+       { num : 3, test: 'Lab/Quiz/Tut', mark: 40}
+   ],
+   slide : [
+       { title : '1 - Binary Codes', link : 'https://drive.google.com/open?id=1dM4UdccAkOo-cFHonVHHdJdjPhu0OhFB'},
+       { title : '2 - Digital K Maps', link : 'https://drive.google.com/open?id=1kJDFkg6uRbMg4IkqmgiJ-UhFQLDzUwcL'},
+       { title : '3 - Combinational Circuits', link : 'https://drive.google.com/open?id=1SsuOl2V2vWIgvVKwr7gkcgiFwFPskDtp'},
+       { title : '4 - Combinational Circuits 2', link : 'https://drive.google.com/open?id=1iCyEeVqVzUgiUKBuoWF_eYjYpRiNjD8j'},
+       { title : '5 - Combinational Circuits 3', link : 'https://drive.google.com/open?id=1vPeUxovHANrDntRppjceSZptDf8ipgM2'},
+       { title : '6 - Combinational Circuits 4', link : 'https://drive.google.com/open?id=1Ya9Qo2Mm3xoO0d_xNvADQmVK-dQtWTkN'},
+       { title : '7 - Analysis of SSC', link : 'https://drive.google.com/open?id=1SLODUviYn-MTGPoBfP310MBZ9WvZ9ctx'},
+       { title : '8 - Counters', link : 'https://drive.google.com/open?id=1DqUJkJu1NB9rPD7DjhCWz6zyTTBQCiKk'},
+       { title : '9 - Flip Flop and Latch', link : 'https://drive.google.com/open?id=1jid5HMU03Pp1zqJ0XeBjNdUa2FmuHl9G'},
+       { title : '10 - Register', link : 'https://drive.google.com/open?id=17fKFH_gWhvbVz400OI99a-D-IWb8GEjP'},
+       { title : '11 - Latches and FlipFlips', link : 'https://drive.google.com/open?id=1hKRtB98c7vN2hWgHifRmdcWRoNa3NlJi'},
+       { title : '12 - Seq. Counters and Circuits', link : 'https://drive.google.com/open?id=1Dh1v764t4u8RUQimSaywPA5kk4X_Cptc'},
+       { title : '13 - Seq. Circuits and Registers', link : 'https://drive.google.com/open?id=1tidcPwQMDJ1adACWwcs_NkIJI1bn-Gj_'},
+       { title : '14 - Seq. Circuits 4', link : 'https://drive.google.com/open?id=1YY_S6-atO-wJR4VTaTy74Q5u-BlZRzOl'},
+       { title : '15 - Seq. Circuits 5', link : 'https://drive.google.com/open?id=1xtlWDc5e3BOb0IIEDGezK1RpqvfKripu'},
+       { title : '16 - Seq. Circuits 4', link : 'https://drive.google.com/open?id=1gxI8pMo5Chk6o7NbUHfVMkLS61TIm2MA'},
+       { title : '17 - Seq. Circuits 5', link : 'https://drive.google.com/open?id=1erDbfy9aNdtWwCv0yYCYAYvuhGqLzScE'}
+
+   ],
+   sheet : [
+       { title : 'Tutorial Sheet 1', link : 'https://drive.google.com/open?id=1DJGfEyPG2QhmwUE9BP79KZ1NMUUn60EQ'},
+       { title : 'Tutorial Sheet 2', link : 'https://drive.google.com/open?id=1cIIMJ2K8Lsmz0XTLJ89hKCTHWbWfNqDB'},
+       { title : 'Tutorial Sheet 3', link : 'https://drive.google.com/open?id=1TnSaH9LkqS5mL8QLck84vZ5NDHaZlcyM'},
+   ],
+
+   lab : [
+       { title : 'Lab 1-2', link : 'https://drive.google.com/open?id=1Zm6kOg4vFFWjXxPcXx05qd9tFsLtGana'},
+
+   ],
+   paper : [
+       { title : '2017 EndSem', link : 'https://drive.google.com/open?id=1mjpy-RuMlrHYva21JuLRRF13RAecJkMO'},
+       { title : '2016 EndSem', link : 'https://drive.google.com/open?id=1N7_qU3cYk5bg7Ugkz9I1gBXh1ozlxb2i'},
+       { title : '2017 MidSem', link : 'https://drive.google.com/open?id=1YW1NOiJ-xIqSYRyuWZ2i7oOMz345m_je'},
+       { title : '2016 MidSem', link : 'https://drive.google.com/open?id=1lsh2bV6SRGwgW0rEvk5jNYvrrq-jnJPk'}
+   ],
+};
+
+//UCS406 Data Structures & Algorithms
+var ucs406 = {
+    name : 'UCS406 Data Structures & Algorithms',
+    code : 'UCS406',
+    credit : 6.0,
+    ltp : 'L-3 &nbsp;&nbsp;&nbsp; T-0 &nbsp;&nbsp;&nbsp; P-4',
+    handout : 'https://drive.google.com/open?id=1vbTXzV0-s1Fvm5L2ZQ06onsTFn-B6JWr',
+    marks : [
+        { num : 1, test: 'MST', mark: 25},
+        { num : 2, test: 'EST', mark: 40},
+        { num : 3, test: 'Lab/Quiz/Project', mark: 35}
+    ],
+    lab : [
+        { title : 'Lab Assignment 1 to 4', link : 'https://drive.google.com/open?id=1ujslnyzHQw9Eyew3TaM8897i9kFGxUUy'},
+        { title : 'Assignment Solutions', link : 'https://drive.google.com/open?id=15DqQ03lsf0NrQ1VNxX8Tjtq9904A7_Wl'},
+    ],
+    paper : [
+        { title : '2016 MidSem', link : 'https://drive.google.com/open?id=1xAtfjOtWvJEpK9PlmsCx32-VSvHrO4sJ'}
+    ],
+    book : [
+        { title : 'Introduction to Algorithms' , link : 'https://drive.google.com/open?id=1RPjIVVHGqIrPf7V0ov-iy0rKXZYXmcP5'},
+        { title : 'Data Structures with C' , link : 'https://drive.google.com/open?id=1NdULNNis3sXaggVNk6cBbFfwa8_6xGzk'},
+        { title : 'Schaum Series ebook' , link : 'https://drive.google.com/open?id=1Jmt2aPszLE-Mb5YxktS8YD1GqbTQ7O7m'},
+        { title : 'Data Structure with Java' , link : 'https://drive.google.com/open?id=1fmuZJgCaxi4gLD6747JC5C1VFmKPk68W'},
+        { title : 'DS & Algo Made Easy Narsimha' , link : 'https://drive.google.com/open?id=18G7eibLJ2L8zscGv4sGzgGusX32ouBZQ'}
+    ],
+    slide :[
+        { title : 'New Slides', link : 'https://drive.google.com/open?id=1f8y0aUjWEQCSbqGc5rrugMwDbFkJZHtU'},
+        { title : 'Old Slides', link : 'https://drive.google.com/open?id=1liC0SYb4XzEnvSVL8pk0c8KmXyyzFD7F'}
+    ],
+    note : [
+        { title : 'Data Structures Full', link : 'https://drive.google.com/open?id=16S3YCTDcEQtVeOZult9ApiM3z9c8qbUC'},
+        { title : 'Data Structures', link : 'https://drive.google.com/open?id=14eYfA5TEIMQpk4-aEQJhVoAM1eSlek0v'},
+        { title : 'Hashing Notes', link : 'https://drive.google.com/open?id=1a_hr3Vw6Nx5IG52ucbIfSIiU5xnNVKMt'}
+    ]
+};
