4c95f6582e863d835f5514f389ba49b9873ee4c1
jetfreakk <33203722+jetfreakk@users.noreply.github.com>
2018-08-23 17:05:20 +0530

    UES012 Upload

--- 2ndYear/js/2ndYearSubjects.js
+++ 2ndYear/js/2ndYearSubjects.js
@@ -1,20 +1,20 @@
 //////////////////////////////// Subjects Information ////////////////////////////////
 
 // 2nd Year
-//Full Subject Name
-var SubjectCode_Small_letters = {
-    name : '',
-    code : '',
-    credit : 0,
-    ltp : 'L- &nbsp;&nbsp;&nbsp; T- &nbsp;&nbsp;&nbsp; P-',
+//UES012 Engineering Materials
+var ues012 = {
+    name : 'UES012 Engineering Materials',
+    code : 'UES012',
+    credit : 4.5,
+    ltp : 'L-3 &nbsp;&nbsp;&nbsp; T-1 &nbsp;&nbsp;&nbsp; P-2',
     handout : 'url',
     marks : [
-        { num : 1, test: 'MST', mark: 0},
-        { num : 2, test: 'EST', mark: 0},
-        { num : 3, test: 'Lab/Quiz/Project', mark: 0}
+        { num : 1, test: 'MST', mark: 25},
+        { num : 2, test: 'EST', mark: 35},
+        { num : 3, test: 'Lab/Quiz/Project', mark: 40}
     ],
     sheet : [
-        { title : 'Title', link : 'url'}
+        { title : 'Title', link : '', sollink : '', status : 'NAV'},
     ],
     slide : [
         { title : 'Title', link : 'url'}
