cf72ee42ed1e564c0e1f976e8e7af8828575eda1
jetfreakk <33203722+jetfreakk@users.noreply.github.com>
2018-08-27 17:59:04 +0530

    UES012

--- 2ndYear/js/2ndYearSubjects.js
+++ 2ndYear/js/2ndYearSubjects.js
@@ -7,19 +7,19 @@ var ues012 = {
     code : 'UES012',
     credit : 4.5,
     ltp : 'L-3 &nbsp;&nbsp;&nbsp; T-1 &nbsp;&nbsp;&nbsp; P-2',
-    handout : 'url',
+    handout : 'https://drive.google.com/open?id=1EwzMkVRfhWCgHTpV1ClH4-4q6Gmv9FKq',
     marks : [
         { num : 1, test: 'MST', mark: 25},
         { num : 2, test: 'EST', mark: 35},
         { num : 3, test: 'Lab/Quiz/Project', mark: 40}
     ],
     sheet : [
-        { title : 'Tutorial Sheet 1-2', link : '', sollink : '', status : 'NAV'},
-        { title : 'Tutorial Sheet 3-4', link : '', sollink : '', status : 'NAV'},
-        { title : 'Tutorial Sheet 5', link : '', sollink : '', status : 'NAV'},
-        { title : 'Tutorial Sheet 6', link : '', sollink : '', status : 'NAV'},
-        { title : 'Tutorial Sheet 7', link : '', sollink : '', status : 'NAV'},
-        { title : 'Tutorial Sheet 8-9', link : '', sollink : '', status : 'NAV'}
+        { title : 'Tutorial Sheet 1-2', link : 'https://drive.google.com/open?id=1sKPLrT9Qn4RHkZIqQ-RYOlDS0M2h3ZM7', sollink : '', status : 'NAV'},
+        { title : 'Tutorial Sheet 3-4', link : 'https://drive.google.com/open?id=1IWdyzFlJaTYU_87Ya64y8j50_HIbl_D7', sollink : '', status : 'NAV'},
+        { title : 'Tutorial Sheet 5', link : 'https://drive.google.com/open?id=1EkIKA0Trqr76a2JkCIcILRo4DHKv-uXb', sollink : '', status : 'NAV'},
+        { title : 'Tutorial Sheet 6', link : 'https://drive.google.com/open?id=1ZECUX4iAjUeMEVo1ArWC6WwjtuAfEeQ1', sollink : '', status : 'NAV'},
+        { title : 'Tutorial Sheet 7', link : 'https://drive.google.com/open?id=16jDUuIbWjy-1FV4oLqxUy87M7ODOplLJ', sollink : '', status : 'NAV'},
+        { title : 'Tutorial Sheet 8-9', link : 'https://drive.google.com/open?id=1zQyUkn9hiQW3A-mZvj5BxosdBp2r8o3J', sollink : '', status : 'NAV'}
     ],
     slide : [
         { title : 'Title', link : 'url'}
